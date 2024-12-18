f410c80ab1b87880261ebd2dd4d3838ccb6eea84
Jose Segura <josed.segurav@gmail.com>
2024-12-17 21:29:37 -0600

    project page updated with json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+module.exports = {
 
+  images: {
+
+    domains: ['drive.usercontent.google.com', 'drive.google.com'] // Add 'drive.google.com' to allowed domains
+
+  }
+
+}
 const nextConfig: NextConfig = {
   /* config options here */
+
 };
 
 export default nextConfig;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,34 +1,48 @@
 import Header from "@/components/header";
-import { promises as fs} from 'fs';
+import projects from "../api/projects.json";
+import Image from "next/image";
 
-export default async function Portofolio() {
-    const path = process.cwd() + "/app/projects.json";
-    const file = await fs.readFile(path, "utf8");
-    const projects = JSON.parse(file);
-
-
-
-    return (
+export default function Portofolio() {
+  console.log(projects[0].images);
+  return (
+    <>
       <div>
         <main>
           <Header></Header>
           <section role="search" className="py-12 bg-white">
-        <div className="container mx-auto">
-            <h1 className="text-3xl font-bold text-center mb-8">Project Showcase</h1>
-             <div className="text-center mb-8">
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">All</button>
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">Residential</button>
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">Commercial</button>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                {
-                    projects.map((project) => {
-                        return (
-                <a className="block" href="#project1">{project.id}
-                <img id="imgProject1" src="./" alt="3M Project" className="w-full h-auto rounded shadow"/>
-            </a>
-                    )})
-                }
+            <div className="container mx-auto">
+              <h1 className="text-3xl font-bold text-center mb-8">
+                Project Showcase
+              </h1>
+              <div className="text-center mb-8">
+                {/* <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">
+                  All
+                </button>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">
+                  Residential
+                </button>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded-full mr-4">
+                  Commercial
+                </button> */}
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                {projects.map((project) => (
+                  <div key={project.id}>
+                    <a className="block" href="#project1">
+                      {project.name}
+
+                      <Image
+                        src={project.images[0].url}
+                        width={500}
+                        height={500}
+                        alt="3M Project"
+                        className="w-full h-auto rounded shadow"
+                      ></Image>
+                    </a>
+                  </div>
+                ))}
+
+                {/*                 
                 <a className="block" href="#project1">
                     <img id="imgProject1" src="./" alt="3M Project" className="w-full h-auto rounded shadow"/>
                 </a>
@@ -46,11 +60,12 @@ export default async function Portofolio() {
                 </a>
                 <a className="block" href="#">
                     <img id="imgProject6" alt="Commercial Project 3" className="w-full h-auto rounded shadow"/>
-                </a>
+                </a> */}
+              </div>
             </div>
-        </div>
-    </section>
+          </section>
         </main>
       </div>
-    );
-  }
+    </>
+  );
+}
